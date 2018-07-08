f47c39750ccf580d730b9944ed5bb225cff71d25
Sean Warnock <swarnock@warnocksolutions.com>
2018-07-07 21:28:13 -0700

    Initial commit

---
+++ Assignment4.cpp
@@ -0,0 +1,40 @@
+/*
+Sean Warnock
+CSCI 14 Summer 2018
+Assignment 4 due 7/12/2018
+https://github.com/seanwarnock/CSCI14-Assignment4.git
+
+1) The logic should allow the user to enter as many
+records as they wish, computer Gross Payu and Net Pay.
+Write the data to the data file, Personel.dat.  Put a single
+ blank between each data field to as as a default delimiter.
+
+2) Retrieve each record.  Read a record and check for the EOF.  Since you really don't
+know how many records are in the data file, use a while loop.
+The loop will read the record at the end of the loop
+
+3) Paginate the output information.
+
+Be sure to format appropriate values as currency.  See if you can
+format the hours as decimal with 1 decimal position (i.e. 40 hours
+as 40.0 hours).  Use a While loop around the main logic to let the
+user keep adding or reading instead of one pass through the entire
+program logic.
+
+*/
+#include <time.h>
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#ifdef _WIN32
+  #include <windows.h>
+  #include <winsock2.h>
+#endif
+#ifdef __linux__
+
+#endif
+
+int main ()
+{
+
+}
